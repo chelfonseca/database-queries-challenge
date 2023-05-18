2c2f56a5b3a391e297d25dc92e3bd75c38575250
Michel Fonseca <michelbeserradafonseca@gmail.com>
2023-05-18 14:10:19 -0300

    Solved but Its necessary review later

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,26 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
+    return await this.repository.findOneOrFail({
+      relations: ['games'],
+      where: {
+        id: user_id
+      }
+    });
     // Complete usando ORM
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query("SELECT * FROM users ORDER BY first_name ASC"); 
+    // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository
+    .query("SELECT * FROM users AS u WHERE u.first_name ILIKE LOWER($1) AND u.last_name ILIKE LOWER($2)", [first_name, last_name]); 
+    // Complete usando raw query
   }
 }
